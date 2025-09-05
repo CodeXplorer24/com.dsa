67e08627ed209177615731f0a559c7de137e48bf
JyotishIJ <anandjyotish4@gmial.com>
2025-09-06 00:35:52 +0530

    new Sum.java file added

---
+++ com/dsa/recursion/Sum.java
@@ -0,0 +1,14 @@
+package com.dsa.recursion;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(sumOfNum(5));
+    }
+    static int sumOfNum (int n) {
+
+        if (n <= 1){
+            return 1;
+        }
+        return n + sumOfNum(n-1);
+    }
+}
